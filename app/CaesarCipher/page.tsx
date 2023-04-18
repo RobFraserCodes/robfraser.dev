ab59ab94181f374fba612d34bca5895fed01bea8
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-04-18 18:29:00 +0100

    Update page.tsx

--- app/CaesarCipher/page.tsx
+++ app/CaesarCipher/page.tsx
@@ -4,14 +4,14 @@ import React, { useState } from 'react';
 
 function CaesarCipher() {
   const [message, setMessage] = useState('');
-  const [shift, setShift] = useState(0);
+  const [shift, setShift] = useState('');
   const [encryptedMessage, setEncryptedMessage] = useState('');
   const [decryptedMessage, setDecryptedMessage] = useState('');
-
-  const handleChange = (setter: React.Dispatch<React.SetStateAction<string>>) => (event: React.ChangeEvent<HTMLInputElement>) => {
+  
+  const handleChange = (setter) => (event) => {
     setter(event.target.value);
   };
-
+  
   const encrypt = () => {
     const parsedShift = parseInt(shift);
     const encrypted = message
@@ -20,11 +20,11 @@ function CaesarCipher() {
         const code = char.charCodeAt(0);
         if (code >= 65 && code <= 90) {
           return String.fromCharCode(((code - 65 + parsedShift) % 26) + 65);
-        }
-        if (code >= 97 && code <= 122) {
+        } else if (code >= 97 && code <= 122) {
           return String.fromCharCode(((code - 97 + parsedShift) % 26) + 97);
+        } else {
+          return char;
         }
-        return char;
       })
       .join('');
     setEncryptedMessage(encrypted);
@@ -32,60 +32,35 @@ function CaesarCipher() {
   
   const decrypt = () => {
     const parsedShift = parseInt(shift);
-    const decrypted = message
+    const decrypted = encryptedMessage
       .split('')
       .map((char) => {
         const code = char.charCodeAt(0);
         if (code >= 65 && code <= 90) {
-          return String.fromCharCode(((code - 65 - parseInt(shift, 10) + 26) % 26) + 65);
+          return String.fromCharCode(((code - 65 - parsedShift + 26) % 26) + 65);
+        } else if (code >= 97 && code <= 122) {
+          return String.fromCharCode(((code - 97 - parsedShift + 26) % 26) + 97);
+        } else {
+          return char;
         }
-        if (code >= 97 && code <= 122) {
-          return String.fromCharCode(((code - 97 - parseInt(shift, 10) + 26) % 26) + 97);
-        }
-        return char;
       })
       .join('');
     setDecryptedMessage(decrypted);
   };
   
-  
-  
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold mb-8">Caesar Cipher</h1>
-      <h3>A wee demo site to encrypt and decrypt messages.</h3>
-      <div className="w-96 bg-white rounded-lg p-6 shadow-lg">
-        <input
-          type="text"
-          value={message}
-          onChange={handleChange(setMessage)}
-          className="w-full px-3 py-2 mb-4 border rounded-lg focus:outline-none focus:ring focus:border-blue-300 text-slate-900"
-          placeholder="Enter message"
-        />
-        <input
-          type="number"
-          value={shift}
-          onChange={handleChange(setShift)}
-          className="w-full px-3 py-2 mb-4 border rounded-lg focus:outline-none focus:ring focus:border-blue-300 text-slate-900"
-          placeholder="Enter shift"
-        />
-        <button
-          onClick={encrypt}
-          className="w-full mb-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300"
-        >
-          Encrypt
-        </button>
-        <div className="mb-4 text-slate-900">{encryptedMessage}</div>
-        <button
-          onClick={decrypt}
-          className="w-full mb-4 px-4 py-2 bg-green-500 text-white font-semibold rounded-lg focus:outline-none focus:ring-2 focus:ring-green-300"
-        >
-          Decrypt
-        </button>
-        <div className='text-slate-900'>{decryptedMessage}</div>
-      </div>
+    <div>
+      <input type="text" placeholder="Enter message to encrypt" onChange={handleChange(setMessage)} />
+      <input type="text" placeholder="Enter shift value" onChange={handleChange(setShift)} />
+      <button onClick={encrypt}>Encrypt</button>
+      <p>Encrypted message: {encryptedMessage}</p>
+      <input type="text" placeholder="Enter message to decrypt" onChange={handleChange(setEncryptedMessage)} />
+      <input type="text" placeholder="Enter shift value" onChange={handleChange(setShift)} />
+      <button onClick={decrypt}>Decrypt</button>
+      <p>Decrypted message: {decryptedMessage}</p>
     </div>
   );
+  
 }
 
 export default CaesarCipher;
