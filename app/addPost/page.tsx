810ed8c88f32bddcebc85165e87ed35971a8a6e3
Code Inverness <hello@rfraser.co.uk>
2023-06-04 21:09:57 +0100

    page

--- app/addPost/page.tsx
+++ app/addPost/page.tsx
@@ -9,19 +9,43 @@ export default function AddPost() {
   const [author, setAuthor] = useState('');
   const [content, setContent] = useState('');
   const [tags, setTags] = useState('');
+  const [image, setImage] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     
-    const { data, error } = await supabase
-      .from('blog_posts')
-      .insert([
-        {
-          title,
-          author,
-          content,
-        },
-      ]);
+  // Upload the image to Supabase Storage
+  const filePath = `posts/${image.name}`;
+  let { error: uploadError } = await supabase.storage
+    .from('images')
+    .upload(filePath, image);
+
+  if (uploadError) {
+    console.error('Error uploading image: ', uploadError);
+    return;
+  }
+
+  // Get the uploaded file url
+  let { publicURL, error: urlError } = supabase.storage
+    .from('images')
+    .getPublicUrl(filePath);
+
+  if (urlError) {
+    console.error('Error getting image URL: ', urlError);
+    return;
+  }
+
+  // Now save the post in the database, including the image url
+  const { data, error } = await supabase
+    .from('blog_posts')
+    .insert([
+      {
+        title,
+        author,
+        content,
+        image_url: publicURL,  // Save the image URL in the database
+      },
+    ]);
     
     if (error) console.error('Error inserting post: ', error);
     
@@ -50,11 +74,12 @@ export default function AddPost() {
     setAuthor('');
     setContent('');
     setTags('');
+    setImage(null);
   };
 
   return (
     <div className='bg-off-white'>
-    <div className='h-screen flex items-center justify-center bg-off-white container mx-auto px-8'>
+    <div className='h-screen flex items-center justify-center px-8'>
         <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center w-full'>
         <h2 className='mb-4'>Add a Post</h2>
         <div className='flex flex-col mb-4 w-full'>
@@ -74,6 +99,12 @@ export default function AddPost() {
             <textarea 
             className='p-2 rounded-md mb-4 w-full'
             rows={10} value={content} onChange={e => setContent(e.target.value)} placeholder="Content" required />
+        </div>
+        <div className='flex flex-col mb-4 w-full'>
+            <label className='justify-start text-gray mb-2'>Image</label>
+            <input 
+            className='p-2 rounded-md mb-4 w-full'
+            type="file" onChange={e => setImage(e.target.files[0])} required />
         </div>
         <div className='flex flex-col mb-4 w-full'>
             <label className='justify-start text-gray mb-2'>Tags</label>
--- app/lib/pocketbase.tsx
+++
@@ -1,5 +0,0 @@
-import pocketbase from 'pocketbase';
-
-const pb = new pocketbase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
-
-export default pb;
