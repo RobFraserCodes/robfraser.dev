98cb9957555c736805ccc6762711e9a1b11ce995
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-06-05 21:53:29 +0100

    change addPost page

--- app/addPost/page.tsx
+++ app/addPost/page.tsx
@@ -1,44 +1,48 @@
-// pages/addPost.js
 'use client';
 
 import { useState } from 'react';
 import { supabase } from '@/utils/supabaseClient';
 
 export default function AddPost() {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [blurb, setBlurb] = useState('');
-  const [slug, setSlug] = useState('');
-  const [content, setContent] = useState('');
-  const [tags, setTags] = useState('');
-  const [image, setImage] = useState(null);
+  const [title, setTitle] = useState<string | null>('');
+  const [author, setAuthor] = useState<string | null>('');
+  const [blurb, setBlurb] = useState<string | null>('');
+  const [slug, setSlug] = useState<string | null>('');
+  const [content, setContent] = useState<string | null>('');
+  const [tags, setTags] = useState<string | null>('');
+  const [image, setImage] = useState<File | null>(null);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    
-  // Upload the image to Supabase Storage
-  const filePath = `posts/${image.name}`;
-  let { error: uploadError } = await supabase.storage
+
+    if (!title || !author || !content || !tags || !image) {
+      console.error('All fields are required');
+      return;
+    }
+
+    // Upload the image to Supabase Storage
+    const filePath = `posts/${image.name}`;
+    let { error: uploadError } = await supabase.storage
     .from('images')
     .upload(filePath, image);
 
-  if (uploadError) {
-    console.error('Error uploading image: ', uploadError);
-    return;
-  }
+    if (uploadError) {
+      console.error('Error uploading image: ', uploadError);
+      return;
+    }
 
-  // Get the uploaded file url
-  let { publicURL, error: urlError } = supabase.storage
+    // Get the uploaded file url
+    let publicURL = supabase.storage
     .from('images')
     .getPublicUrl(filePath);
 
-  if (urlError) {
-    console.error('Error getting image URL: ', urlError);
-    return;
-  }
+    if (!publicURL) {
+      console.error('Error getting image URL');
+      return;
+    }
 
-  // Now save the post in the database, including the image url
-  const { data, error } = await supabase
+    // Now save the post in the database, including the image url
+    const { data, error } = await supabase
     .from('blog_posts')
     .insert([
       {
@@ -50,23 +54,33 @@ export default function AddPost() {
     ]);
     
     if (error) console.error('Error inserting post: ', error);
+
+    if (!data) {
+      console.error('No data received after inserting post');
+      return;
+    }
     
     // Add each tag to the tags table and link it to the new post
     const tagArray = tags.split(','); // Split tags by comma
     for (const tagName of tagArray) {
       const { data: tag, error: tagError } = await supabase
-        .from('tags')
-        .insert([
-          { tag_name: tagName.trim() },
-        ], { upsert: true }); // If the tag already exists, it won't create a new one
+      .from('tags')
+      .insert([
+        { tag_name: tagName.trim() },
+      ], { upsert: true }); // If the tag already exists, it won't create a new one
         
       if (tagError) console.error('Error inserting tag: ', tagError);
+
+      if (!tag) {
+        console.error('No tag data after insert');
+        return;
+      }
       
       const { data: postTag, error: postTagError } = await supabase
-        .from('post_tags')
-        .insert([
-          { post_id: data[0].id, tag_id: tag[0].id },
-        ]);
+      .from('post_tags')
+      .insert([
+        { post_id: data[0].id, tag_id: tag[0].id },
+      ]);
         
       if (postTagError) console.error('Error inserting post-tag: ', postTagError);
     }
@@ -80,55 +94,6 @@ export default function AddPost() {
   };
 
   return (
-    <div className='bg-off-white '>
-    <div className='flex items-center justify-center px-20 max-w-5xl mx-auto'>
-        <form onSubmit={handleSubmit} className='my-8 flex flex-col items-center justify-center w-full'>
-        <h2 className='mb-4'>Add Post</h2>
-        <div className='flex flex-col mb-4 w-full'>
-            <label className='justify-start text-gray mb-2'>Post Title</label>
-            <input 
-            className='p-2 rounded-md mb-4 w-full'
-            type="text" value={title} onChange={e => setTitle(e.target.value)} placeholder="Title" required />
-        </div>
-        <div className='flex flex-col mb-4 w-full'>
-            <label className='justify-start text-gray mb-2'>Author</label>
-            <input 
-            className='p-2 rounded-md mb-4 w-full'
-            type="text" value={author} onChange={e => setAuthor(e.target.value)} placeholder="Author" required />
-        </div>
-        <div className='flex flex-col mb-4 w-full'>
-            <label className='justify-start text-gray mb-2'>Blurb</label>
-            <input 
-            className='p-2 rounded-md mb-4 w-full'
-            type="text" value={blurb} onChange={e => setBlurb(e.target.value)} placeholder="Author" required />
-        </div>
-        <div className='flex flex-col mb-4 w-full'>
-            <label className='justify-start text-gray mb-2'>Slug</label>
-            <input 
-            className='p-2 rounded-md mb-4 w-full'
-            type="text" value={slug} onChange={e => setSlug(e.target.value)} placeholder="Author" required />
-        </div>
-        <div className='flex flex-col mb-4 w-full'>
-            <label className='justify-start text-gray mb-2'>Content</label>
-            <textarea 
-            className='p-2 rounded-md mb-4 w-full'
-            rows={10} value={content} onChange={e => setContent(e.target.value)} placeholder="Content" required />
-        </div>
-        <div className='flex flex-col mb-4 w-full'>
-            <label className='justify-start text-gray mb-2'>Image</label>
-            <input 
-            className='p-2 rounded-md mb-4 w-full'
-            type="file" onChange={e => setImage(e.target.files[0])} required />
-        </div>
-        <div className='flex flex-col mb-4 w-full'>
-            <label className='justify-start text-gray mb-2'>Tags</label>
-            <input 
-            className='p-2 rounded-md mb-4 w-full'
-            type="text" value={tags} onChange={e => setTags(e.target.value)} placeholder="Tags (comma-separated)" required />
-        </div>
-        <button type="submit" className='bg-primary hover:bg-primary-dark text-white p-2 rounded-md'>Add Post</button>
-        </form>
-    </div>
-    </div>
+    // The rest of your JSX code here
   );
 }
