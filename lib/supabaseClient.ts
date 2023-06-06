d298414b977da1fb7c8aa0893797a515e950790d
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-06-06 20:44:52 +0100

    error

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
+interface SupabaseClient {
+    persistSession: boolean;
+}
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
