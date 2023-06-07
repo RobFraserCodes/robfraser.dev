1ad2bda83b9f4b4131fb2d8f1b4b22dbd0c6ebbf
Code Inverness <hello@rfraser.co.uk>
2023-06-07 11:38:30 +0100

    serviceSelection update

--- app/build-my-app/components/ServiceSelection.tsx
+++ app/build-my-app/components/ServiceSelection.tsx
@@ -2,40 +2,43 @@
 
 import { useState } from 'react';
 
-function ServiceSelection({ handleServiceSelection }) {
+interface ServiceSelectionProps {
+    handleServiceSelection: (service: string) => void;
+}
+
+function ServiceSelection({ handleServiceSelection }: ServiceSelectionProps) {
     const [selectedService, setSelectedService] = useState('');
-  
-    const handleSelection = (service) => {
-      setSelectedService(service);
-      handleServiceSelection(service);
+
+    const handleSelection = (service: string) => {
+        setSelectedService(service);
+        handleServiceSelection(service);
     };
-  
+
     return (
         <div>
             <h3 className='mb-14'>What type of service do you need?</h3>
             <div className="flex justify-around">
                 <div className="text-center">
-                <button className="mt-2 block w-full p-3 rounded bg-primary text-white font-semibold focus:outline-none"
+                    <button className="mt-2 block w-full p-3 rounded bg-primary text-white font-semibold focus:outline-none"
                         onClick={() => handleSelection('UX Design')}>
-                    UX Design
-                </button>
+                        UX Design
+                    </button>
                 </div>
                 <div className="text-center">
-                <button className="mt-2 block w-full p-3 rounded bg-primary text-white font-semibold focus:outline-none"
+                    <button className="mt-2 block w-full p-3 rounded bg-primary text-white font-semibold focus:outline-none"
                         onClick={() => handleSelection('Development')}>
-                    Development
-                </button>
+                        Development
+                    </button>
                 </div>
                 <div className="text-center">
-                <button className="mt-2 block w-full p-3 rounded bg-primary text-white font-semibold focus:outline-none"
+                    <button className="mt-2 block w-full p-3 rounded bg-primary text-white font-semibold focus:outline-none"
                         onClick={() => handleSelection('Both')}>
-                    Both
-                </button>
+                        Both
+                    </button>
                 </div>
             </div>
         </div>
     );
-  }
-  
-  export default ServiceSelection;
-  
+}
+
+export default ServiceSelection;
