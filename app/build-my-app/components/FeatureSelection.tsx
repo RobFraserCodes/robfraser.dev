5425dbf18a280669a4fa91ce2c6b67ab555589e5
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-06-13 20:21:21 +0100

    added alt

--- app/build-my-app/components/FeatureSelection.tsx
+++ app/build-my-app/components/FeatureSelection.tsx
@@ -31,7 +31,7 @@ function FeatureSelection({ handleFeatureSelection }: FeatureSelectionProps) {
   return (
     <div className="flex flex-col-reverse md:flex-row mx-8">
       <div className="md:w-1/3 flex flex-col md:flex-row justify-around mx-auto pt-20">
-        <Image src={coding} width={300} height={300} />
+        <Image src={coding} width={300} height={300} alt='A man coding.' />
       </div>
       <div className="md:w-2/3 flex flex-col">
         <div className="mb-8">
