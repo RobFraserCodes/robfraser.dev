59d7125aec572bd7679fbef929446b0da47a2da2
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-06-27 08:16:45 +0100

    err

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 import profile from '../public/profile.png';
 import Skew from './subComponents/skew';
 
-const ExplodingShape = ({ x, y }) => {
+const ExplodingShape = ({ x, y }: { x: number; y: number }) => {
   const [isExploded, setIsExploded] = useState(false);
 
   const handleClick = () => {
