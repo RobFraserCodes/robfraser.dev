17a70ab1d965d04281416048036cee5fd98155a9
Code Inverness <hello@rfraser.co.uk>
2023-07-06 23:26:33 +0100

    err

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,33 @@
-import Footer from '@/components/Footer'
-import Head from 'next/head'
-import Script from 'next/script'
-import './globals.css'
+import Footer from '@/components/Footer';
+import Head from 'next/head';
+import Script from 'next/script';
+'use client';
+
+import useEffect from 'react';
+import './globals.css';
 
 export const metadata = {
   title: 'Rob Fraser UX Designer and Developer',
   description: 'A personal portfolio and blog for Rob Fraser',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'AW-11236894671');
+  }, []);
+
   return (
-    <html lang="en" className='bg-off-white dark:bg-dark'>
-        <Head>
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-9XHZF56DJJ"
-        />
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-9XHZF56DJJ');
-            `,
-          }}
-        />
-        </Head>
-        <body className='flex flex-col text-black'>
+    <>
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11236894671"></script>
+      </Head>
+      <body className="flex flex-col text-black">
         {children}
         <Footer />
       </body>
-    </html>
-  )
+    </>
+  );
 }
