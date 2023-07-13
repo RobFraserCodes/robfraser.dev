ea69a086133ec861ff419b9fcb2d1860c87cbcb8
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-07-13 23:18:56 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,21 @@
 import Footer from '@/components/Footer';
 import Navbar from '@/components/NavBar';
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Rob Fraser UX Designer and Developer',
   description: 'A personal portfolio and blog for Rob Fraser',
 };
 
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body className="flex flex-col text-black">
+        <Script
+          async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
         <Navbar />
         {children}
         <Footer />
