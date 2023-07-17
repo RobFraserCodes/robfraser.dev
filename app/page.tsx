116aa0b541c9888620b00ec24e77e2e683fed331
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-07-17 09:43:17 +0100

    tweak hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
-import NavBar from "@/components/NavBar";
 import Portfolio from "@/components/Portfolio";
 import Skills from "@/components/Skills";
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,7 +28,7 @@ const Hero = () => {
     <div className="relative bg-off-white dark:bg-dark">
       <section id="hero" className="container flex justify-center mx-auto pb-40">
         <div className="flex flex-col items-center text-black">
-          <h3 className="font-thin dark:text-white bg-gray/20 px-4 py-2 mt-16 mb-4 rounded-full">UX Designer and Frontend Developer</h3>
+          <h3 className=" text-primary dark:text-white bg-primary/10 px-4 py-2 mt-16 mb-4 rounded-full">UX Designer and Frontend Developer</h3>
           <h1 className="dark:text-white bg-gradient-to-r from-primary-light to-secondary-dark bg-clip-text text-transparent mb-16">rob fraser</h1>
           <div
             className="relative w-80 h-80 pt-8"
@@ -36,16 +36,6 @@ const Hero = () => {
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
           >
-            {/* Add interactive circles
-            {[...Array(5)].map((_, i) => (
-              <motion.div
-              key={i}
-              className={`absolute w-20 h-20 bg-primary rounded-full scale-90 hover:scale-110 active:scale-80 transition-all duration-200 ease-in-out`}
-              style={{ top: `${mousePosition.y + i}px`, left: `${mousePosition.x + i}px` }}
-              whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 0.8 }}
-              />
-            ))} */}
             <div className='absolute w-60 h-60 bg-primary/40 rounded-full right-48 top-32'></div>
             <div className='absolute w-36 h-36 bg-secondary/20 rounded-full left-64 top-8'></div>
             <div className='absolute w-16 h-16 bg-secondary rounded-full left-96 top-36'></div>
