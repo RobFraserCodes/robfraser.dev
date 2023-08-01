45b37ec3df3a12e1fb74b4df2ab32ff79ec6c8f0
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-08-01 23:12:50 +0100

    added prisma
    linked prosma postgres

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import prisma from '@/lib/prisma';
 
-export default function PortfolioPage() {
+export default async function PortfolioPage() {
+  const post = await prisma.post.findMany({
+    where: {
+      published: true,
+    },
+  });
+
   return (
-    <div>page</div>
+    <div>
+      <h1>Portfolio</h1>
+      <ul>
+        {post.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
